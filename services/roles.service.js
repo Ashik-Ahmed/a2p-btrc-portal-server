ec39e8a51cadda3d808d42a2e53b1d507f59e97f
ashik-ahmed <ashikahmed121@gmail.com>
2025-01-06 15:43:35 +0600

    update role by id service function created

--- services/roles.service.js
+++ services/roles.service.js
@@ -22,3 +22,43 @@ exports.getRoleByIdService = async (id) => {
     const result = await client.query("SELECT * FROM roles_tbl WHERE role_id = $1", [id]);
     return result.rows[0];
 }
+
+
+exports.updateRoleByIdService = async (id, roleData) => {
+
+    let query = "UPDATE roles_tbl SET ";
+    const conditions = [];
+    const values = [];
+
+    if (roleData?.role_name) {
+        conditions.push(`role_name = $${conditions.length + 1}`);
+        values.push(roleData?.role_name);
+    }
+    if (roleData?.users) {
+        conditions.push(`users = $${conditions.length + 1}`);
+        values.push(roleData?.users);
+    }
+    if (roleData?.page_access) {
+        conditions.push(`page_access = $${conditions.length + 1}`);
+        values.push(roleData?.page_access);
+    }
+    if (roleData?.updated_by) {
+        conditions.push(`updated_by = $${conditions.length + 1}`);
+        values.push(roleData?.updated_by);
+    }
+    if (roleData?.updated_at) {
+        conditions.push(`updated_at = $${conditions.length + 1}`);
+        values.push(roleData?.updated_at);
+    }
+
+    if (conditions.length > 0) {
+        query += conditions.join(", ");
+    }
+
+    query += ` WHERE role_id = $${conditions.length + 1}`;
+    values.push(id);
+
+    const result = await client.query(query, values);
+
+    return result;
+}
