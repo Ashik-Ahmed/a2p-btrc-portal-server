cd62c9d7ba8230017ee772de1359a8ec49e205b0
ashik-ahmed <ashikahmed121@gmail.com>
2025-01-10 23:16:18 +0600

    get role by id query updated

--- services/roles.service.js
+++ services/roles.service.js
@@ -13,13 +13,52 @@ exports.createNewRoleService = async (roleData) => {
 
 
 exports.getAllRoleService = async () => {
-    const result = await client.query("SELECT * FROM roles_tbl");
+    const result = await client.query(`SELECT 
+    r.*,
+    jsonb_agg(
+        jsonb_build_object(
+            'page_id', p.page_id,
+            'title', p.title,
+            'url', p.url
+        )
+    ) AS allowed_pages
+FROM 
+    public.roles_tbl r
+LEFT JOIN 
+    public.pages_tbl p
+ON 
+    p.page_id = ANY(r.page_access::integer[])
+GROUP BY 
+    r.role_id, r.page_access
+    
+ORDER BY 
+    r.role_id ASC`);
     return result.rows;
 }
 
 
 exports.getRoleByIdService = async (id) => {
-    const result = await client.query("SELECT * FROM roles_tbl WHERE role_id = $1", [id]);
+    const result = await client.query(`SELECT 
+    r.*,
+    jsonb_agg(
+        jsonb_build_object(
+            'page_id', p.page_id,
+            'title', p.title,
+            'url', p.url
+        )
+    ) AS allowed_pages
+FROM 
+    public.roles_tbl r
+LEFT JOIN 
+    public.pages_tbl p
+ON 
+    p.page_id = ANY(r.page_access::integer[])
+     WHERE role_id = $1
+GROUP BY 
+    r.role_id, r.page_access
+    
+ORDER BY 
+    r.role_id ASC`, [id]);
     return result.rows[0];
 }
 
@@ -71,11 +110,11 @@ exports.deleteRoleByIdService = async (id) => {
 
 
 exports.createPageService = async (pageData) => {
-    const { page_serial, title, url, visibility, status, created_by, updated_by, updated_at } = pageData;
+    const { page_serial, title, url, child_pages, visibility, status, created_by, updated_by, updated_at } = pageData;
 
-    let query = `INSERT INTO pages_tbl (page_serial, title, url, visibility, status, created_by, updated_by, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
+    let query = `INSERT INTO pages_tbl (page_serial, title, url, child_pages, visibility, status, created_by, updated_by, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`;
 
-    const result = await client.query(query, [page_serial, title, url, visibility, status, created_by, updated_by, updated_at]);
+    const result = await client.query(query, [page_serial, title, url, child_pages, visibility, status, created_by, updated_by, updated_at]);
 
     return result.rows[0];
 }
